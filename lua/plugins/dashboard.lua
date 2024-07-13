ab17fca64b5b6bb99aa218f1133ae1e3edcb7b8e
spectrumzero <lmhspecter@gmail.com>
2024-07-13 20:04:32 +0800

    lazyVim.telescope is deprecated. Please use LazyVim.pick.telescope instead.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -14,6 +14,9 @@ return {
                                                                       
                                                                       
                                                                       
+                                                                      
+                                                                      
+                                                                      
     ]]
 
     logo = string.rep("\n", 8) .. logo .. ""
@@ -30,12 +33,12 @@ return {
         -- stylua: ignore
         center = {
           { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-          -- { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+          { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
           { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
           { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-          { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua LazyVim.pick.config_files()()',                        desc = " Config",          icon = " ", key = "c" },
           { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          -- { action = 'Telescope persisted',                                      desc = " Search sessions", icon = " ", key = "S" },
+          { action = 'Telescope persisted',                                      desc = " Search sessions", icon = " ", key = "S" },
           { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
           { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
           { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
@@ -43,7 +46,10 @@ return {
         footer = function()
           local stats = require("lazy").stats()
           local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          return {
+            "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
+          }
+          -- return { "\n" .. "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
         end,
       },
     }
